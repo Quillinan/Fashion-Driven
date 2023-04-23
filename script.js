d037c18069aacc7415235256965834d68929a282
Quillinan <milton.quillinan1798@gmail.com>
2023-04-22 23:33:08 -0300

    add select cards function

--- script.js
+++ script.js
@@ -14,17 +14,44 @@ function pegarCitacao() {
 
 pegarCitacao();
 
-function selectedCollar() {
-  // const selected = document.querySelector('')
-  console.log('uiui');
+function selectedModel(model) {
+  const selected = document.querySelector('.models');
+  const model_selected = selected.querySelector('.selected');
+  const model_check = selected.querySelector('.selected');
+
+  if (model_selected !== null && model_check !== null) {
+    model_selected.classList.add('unselected');
+    model_selected.classList.remove('selected');
+  }
+
+  model.classList.remove('unselected');
+  model.classList.add('selected');
 }
 
-function selectedModel() {
-  // const selected = document.querySelector('')
-  console.log('uiui');
+function selectedCollar(collar) {
+  const selected = document.querySelector('.collars');
+  const collar_selected = selected.querySelector('.selected');
+  const collar_check = selected.querySelector('.selected');
+
+  if (collar_selected !== null && collar_check !== null) {
+    collar_selected.classList.add('unselected');
+    collar_selected.classList.remove('selected');
+  }
+
+  collar.classList.remove('unselected');
+  collar.classList.add('selected');
 }
 
-function selectedTissue() {
-  // const selected = document.querySelector('')
-  console.log('uiui');
+function selectedTissue(tissue) {
+  const selected = document.querySelector('.tissues');
+  const tissue_selected = selected.querySelector('.selected');
+  const tissue_check = selected.querySelector('.selected');
+
+  if (tissue_selected !== null && tissue_check !== null) {
+    tissue_selected.classList.add('unselected');
+    tissue_selected.classList.remove('selected');
+  }
+
+  tissue.classList.remove('unselected');
+  tissue.classList.add('selected');
 }
